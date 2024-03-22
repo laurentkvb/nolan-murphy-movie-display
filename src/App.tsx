bb2820565848ac6a57efc74e5aa354bc43a6be1b
Laurent Kvb <laurent.kleeringvanbeerenbergh@leaseplan.com>
2024-03-22 23:02:23 +0100

    implement initial movie view

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { getMoviesInvolvingActors, Movie } from "./api";
+import { Fade } from "react-swift-reveal";
+
+import NolanMurphy from "./assets/Nolan-Murphy.png";
 import "./App.css";
-import { getMoviesInvolvingActors } from "./api";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [movies, setMovies] = useState<string[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const textSegments = [
+    "Christopher Nolan",
+    "&",
+    "Cillian Murphy",
+    "Movie Collaborations",
+  ];
 
   useEffect(() => {
-      getMoviesInvolvingActors("Christopher Nolan", "Cillian Murphy")
+    getMoviesInvolvingActors("Christopher Nolan", "Cillian Murphy")
       .then((movies) => {
         setMovies(movies);
         console.log("Movies involving both actors:", movies);
@@ -19,40 +25,82 @@ function App() {
       });
   }, []);
 
-  useEffect(() => {
-    console.log("movies");
-    console.log(movies);
-  }, [movies]);
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={NolanMurphy} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      {movies && (
-        <h2>
-          {Array.from(movies).map((e) => (
-            <h3 style={{ textAlign: "start" }}>{`- ${e}`}</h3>
-          ))}
-        </h2>
-      )}
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        {textSegments.map((segment, index) => (
+          <h1 key={index} style={{ lineHeight: "20px" }}>
+            {segment}
+          </h1>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div
+        style={{
+          marginTop: 100,
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          flexDirection: "row",
+          gap: 10,
+        }}
+      >
+        {movies &&
+          Array.from(movies).map(
+            (
+              {
+                title,
+                poster_path,
+                original_title,
+                release_date,
+                adult,
+                character,
+              },
+              idx,
+            ) => (
+              <Fade delay={500 * idx}>
+                <div
+                  className="card"
+                  style={{
+                    border: "5px solid black",
+                    borderRadius: 5,
+                    width: 300,
+                    height: "100%",
+                    padding: 20,
+                    minHeight: 800,
+                  }}
+                >
+                  <h3
+                    style={{ textAlign: "start", height: 50 }}
+                  >{`- ${title}`}</h3>
+                  {poster_path && (
+                    <img
+                      alt={`${original_title}-poster`}
+                      src={`https://image.tmdb.org/t/p/original${poster_path}`}
+                      style={{ minHeight: 500 }}
+                      width="100%"
+                    />
+                  )}
+                  <div style={{ textAlign: "start", padding: 10 }}>
+                    <p>{`Release Date: ${release_date}`}</p>
+                    <p>{`Adult rated?: ${adult ? "yes" : "no"}`}</p>
+                    {character && <p>{`Character?: ${character}`}</p>}
+                  </div>
+                </div>
+              </Fade>
+            ),
+          )}
+      </div>
     </>
   );
 }
